5db02b6ac2eb9020296854501dbf9ae1c978de8f
Danial Hassan <120138677+danialhassan72@users.noreply.github.com>
2024-08-27 02:55:47 +0500

    Update .py

--- .py
+++ .py
@@ -1,44 +1,46 @@
-print("Let's play Hangman!")
-print(display_hangman(tries))
-print(word_completion)
-print("\n")
+import random
+#import hangman art
+from art import stages, logo
+import hangman_words
+from hangman_words import word_list
 
-while not guessed and tries < 6:
-    guess = input("Please guess a letter or word: ").lower()
-    if len(guess) == 1 and guess.isalpha():
-        if guess in guessed_letters:
-            print("You already guessed the letter", guess)
-        elif guess not in word:
-            print(guess, "is not in the word.")
-            tries += 1
-            guessed_letters.append(guess)
-        else:
-            print("Good job,", guess, "is in the word!")
-            guessed_letters.append(guess)
-            word_as_list = list(word_completion)
-            indices = [i for i, letter in enumerate(word) if letter == guess]
-            for index in indices:
-                word_as_list[index] = guess
-            word_completion = ''.join(word_as_list)
-            if '_' not in word_completion:
-                guessed = True
-    elif len(guess) == len(word) and guess.isalpha():
-        if guess in guessed_words:
-            print("You already guessed the word", guess)
-        elif guess != word:
-            print(guess, "is not the word.")
-            tries += 1
-            guessed_words.append(guess)
-        else:
-            guessed = True
-            word_completion = word
-    else:
-        print("Not a valid guess.")
-    print(display_hangman(tries))
-    print(word_completion)
-    print("\n")
+end_of_game = False
 
-if guessed:
-    print("Congrats, you guessed the word! You win!")
-else:
-    print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
+lives = 6
+
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+print(logo)
+
+#testing code
+# print(f"computer close [chosen_word]")
+
+display = []
+
+for _ in range(word_length):
+    display += "_"
+print(display)
+
+while not end_of_game:
+    guess = input("guess a letter: ").lower()
+    if guess in display:
+        print(f"you have already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+
+    if "-" not in display:
+        end_of_game = True
+        print("you won!")
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"you have left with (lives) lives")
+        if lives == 0:
+            end_of_game = True
+            print("you lose!")
+            print(f"The chosen word was: {chosen_word}")
+        print(stages[lives])
